8ec76912b2f92eb93581b5707c9434a1d258b448
elvizer <gamerprotime777@gmail.com>
2026-01-24 12:19:59 -0500

    [SwerveBase] naming

--- src/main/java/frc/robot/utils/HolonomicController.java
+++ src/main/java/frc/robot/utils/HolonomicController.java
@@ -187,7 +187,7 @@ public class HolonomicController {
    */
   public ChassisSpeeds calculate(ChassisSpeeds baseSpeeds, Pose2d desiredPose, Pose2d currentPose) {
     DogLog.log("Auto/Controller Desired Pose", desiredPose);
-    DogLog.log("Auto/Controller Reference Pose", currentPose);
+    DogLog.log("Auto/Controller Actual Pose", currentPose);
 
     if (!SwerveConstants.ignorePoseTolerance) {
       Translation2d translationError =
