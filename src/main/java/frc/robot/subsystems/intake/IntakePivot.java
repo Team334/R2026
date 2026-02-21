f72dbdce591d79c961f6b37ff9de32985be50e2a
PGgit08 <peter.mty8@gmail.com>
2026-02-20 19:13:38 -0500

    spotless

--- src/main/java/frc/robot/subsystems/intake/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivot.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.intake;
 
 import static edu.wpi.first.units.Units.*;
+
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
