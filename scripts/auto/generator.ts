9a27b30386b64786353b64dab86c7bc63957b42b
PGgit08 <peter.mty8@gmail.com>
2026-03-01 22:37:42 -0500

    added more constraint code, might need to modify makeConstraint a bit

--- scripts/auto/generator.ts
+++ scripts/auto/generator.ts
@@ -8,6 +8,7 @@ import { exec } from "child_process";
 
 // @ts-ignore
 import os from "os";
+import { ConstraintData } from "./choreo/ConstraintDefinitions";
 
 const CHOREO_CLI = os.homedir().replace(/\\/g, "/") + '/AppData/Local/choreo/choreo-cli';
 
@@ -33,8 +34,24 @@ function loadLayout(name: string) : Layout {
     return JSON.parse(layoutData) as Layout;
 }
 
+function shiftConstraints(constraints: Constraint[], waypointOffset: number) : Constraint[] {
+    const shiftedConstraints: Constraint[] = [];
+
+    for (const constraint of constraints) {
+        if (typeof constraint.from === 'number') constraint.from += waypointOffset;
+        if (typeof constraint.to === 'number') constraint.to += waypointOffset;
+
+        shiftedConstraints.push(constraint);
+    }
+
+    return shiftedConstraints;
+}
+
 function buildTrajectory(baseTraj: Trajectory, name: string, side: Side, layout: Location[]) : Trajectory {
     let trajWaypoints: Waypoint<Expr>[] = [];
+    let trajConstraints: Constraint[] = [];
+
+    let waypointOffset: number = 0;
 
     for (const location of layout) {
         const params = locationParams[location];
@@ -42,14 +59,23 @@ function buildTrajectory(baseTraj: Trajectory, name: string, side: Side, layout:
         switch (side) {
             case "L":
                 trajWaypoints.push(...(params.leftWaypoints ?? []));
+                trajConstraints.push(...shiftConstraints(params.constraints ?? [], waypointOffset));
+
+                waypointOffset += params.leftWaypoints?.length ?? 0;
                 break;
     
             case "C":
                 trajWaypoints.push(...(params.centerWaypoints ?? []));
+                trajConstraints.push(...shiftConstraints(params.constraints ?? [], waypointOffset));
+
+                waypointOffset += params.centerWaypoints?.length ?? 0;
                 break;
 
             case "R":
                 trajWaypoints.push(...(params.rightWaypoints ?? []));
+                trajConstraints.push(...shiftConstraints(params.constraints ?? [], waypointOffset));
+
+                waypointOffset += params.rightWaypoints?.length ?? 0;
                 break;
 
             default:
@@ -57,10 +83,14 @@ function buildTrajectory(baseTraj: Trajectory, name: string, side: Side, layout:
         }
     }
 
-    baseTraj.params.waypoints = trajWaypoints;
-    baseTraj.name = side + name;
+    const updatedTraj: Trajectory = baseTraj;
 
-    return baseTraj;
+    updatedTraj.name = side + name;
+
+    updatedTraj.params.waypoints = trajWaypoints;
+    updatedTraj.params.constraints.push(...trajConstraints);
+
+    return updatedTraj;
 }
 
 
@@ -115,6 +145,15 @@ const makeWaypoint = (x: number, y: number, heading: number, fixTranslation: boo
     };
 };
 
+const makeConstraint = (from: number, to: number, data: ConstraintData) : Constraint => {
+    return {
+        from: from,
+        to: to,
+        data: data,
+        enabled: true
+    };
+}
+
 const locationParams: Record<Location, LocationParams> = {
     start: {
         centerWaypoints: [makeWaypoint(3, 4, 0)]
@@ -125,13 +164,17 @@ const locationParams: Record<Location, LocationParams> = {
             makeWaypoint(4.059676647186279, 7.521993637084961, -1.5599661588553948),
             makeWaypoint(7.636203765869141, 6.131853103637695, -1.4711286226200226),
             makeWaypoint(7.501785278320312, 4.563638210296631, -2.0576957311828057),
-            makeWaypoint(4.451730728149414, 7.541596412658691, 0)
+            makeWaypoint(4.586862087249756, 7.546622276306152, -1.5708)
         ]
     },
     trench: {},
     depot: {
         centerWaypoints: [
-            makeWaypoint(0.3876773416996002, 6.857964038848877, -1.5707963267948966)
+            makeWaypoint(0.4917987287044525, 6.870540618896484, -1.5707963267948966),
+            makeWaypoint(0.4917987287044525, 5.075254440307617, -1.5707963267948966)
+        ],
+        constraints: [
+            makeConstraint(0, 1, {type: "KeepInLane", props: {tolerance: toExpr(0.03, "m")}})
         ]
     },
     human: {},
@@ -144,8 +187,11 @@ var baseTraj: Trajectory = {
     params: {
         waypoints: [],
         constraints: [
-            {from: "first", to:"last", data: {type: "KeepOutCircle", props: {x: toExpr(4.6220447067171335, "m"), y: toExpr(6.499967720359564, "m"), r: toExpr(0.7, "m")}}, enabled: true},
-            {from: "first", to:"last", data: {type: "KeepInRectangle", props: {x: toExpr(0, "m"), y: toExpr(0, "m"), w: toExpr(16.541, "m"), h: toExpr(8.0629, "m")}}, enabled: true},
+            {from: "first", data: {type: "StopPoint", props: {}}, enabled: true},
+            {from: "last", data: {type: "StopPoint", props: {}}, enabled: true},
+            {from: "first", to: "last", data: {type: "KeepOutCircle", props: {x: toExpr(4.6220447067171335, "m"), y: toExpr(6.499967720359564, "m"), r: toExpr(0.7, "m")}}, enabled: true},
+            // TODO right side keep out circle
+            {from: "first", to: "last", data: {type: "KeepInRectangle", props: {x: toExpr(0, "m"), y: toExpr(0, "m"), w: toExpr(16.541, "m"), h: toExpr(8.0629, "m")}}, enabled: true}
         ],
         targetDt: toExpr(0.05, "s")
     },
@@ -167,8 +213,6 @@ var baseTraj: Trajectory = {
 var layout: Layout = loadLayout("layout");
 
 var traj : Trajectory = buildTrajectory(baseTraj, layout.name, "C", layout.layout);
-
-// console.log(traj.params.waypoints.length);
 
 saveTrajectory(traj);
 generateTrajectory(traj);
