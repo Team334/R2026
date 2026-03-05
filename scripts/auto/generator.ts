20397ed597b26693fc2ad1aa7caa552a0da85661
PGgit08 <peter.mty8@gmail.com>
2026-03-05 18:52:04 -0500

    added waypoint reflecting and more cli stuff

--- scripts/auto/generator.ts
+++ scripts/auto/generator.ts
@@ -10,6 +10,9 @@ import { exec } from "child_process";
 import os from "os";
 import { ConstraintData } from "./choreo/ConstraintDefinitions";
 
+// @ts-ignore
+import readline from "readline/promises";
+
 const CHOREO_CLI = os.homedir().replace(/\\/g, "/") + '/AppData/Local/choreo/choreo-cli';
 
 // (assume running generator from R2026 dir)
@@ -18,6 +21,20 @@ const TRAJ_DIR: string = DEPLOY_DIR + '/choreo/';
 
 const LAYOUT_DIR: string = DEPLOY_DIR + '/layouts';
 const CONFIG: string = TRAJ_DIR + '/config.chor';
+
+const FIELD_WIDTH = 8.0629;
+
+const rl = readline.createInterface({
+    // @ts-ignore
+    input: process.stdin,
+
+    // @ts-ignore
+    output: process.stdout
+});
+
+async function prompt(q: string) {
+  return await rl.question(q);
+}
 
 function toExpr(val: number, unit: string) : Expr {
   return { exp: `${val} ${unit}`, val: val };
@@ -150,7 +167,7 @@ function generateTrajectory(traj: Trajectory) : void {
 }
 
 type Side = "l" | "c" | "r";
-type GenericLocation = "start" | "neutralbump" | "neutralmiddle" | "trench" | "depot" | "human" | "climb";
+type GenericLocation = "start" | "neutralbump" | "neutralmiddle" | "trench" | "depot" | "climb";
 
 type Location = `${Side}_${GenericLocation}`;
 
@@ -168,6 +185,14 @@ interface Layout {
     name: string,
     layout: Location[]
 }
+
+const reflectWaypoints = (...waypoints: Waypoint<Expr>[]) => {
+    return waypoints.map(waypoint => ({
+        ...waypoint,
+        y: toExpr(FIELD_WIDTH - waypoint.y.val, "m"),
+        heading: toExpr(waypoint.heading.val, "rad")
+    }));
+};
 
 const makeWaypoint = (x: number, y: number, heading: number, fixTranslation: boolean = true, fixHeading: boolean = true) : Waypoint<Expr> => {
     return {
@@ -208,7 +233,10 @@ const locationParams: Record<GenericLocation, LocationParams> = {
     start: {
         leftWaypoints: [
             makeWaypoint(3.419274091720581, 7.474021911621094, 0.0)
-        ]
+        ],
+        rightWaypoints: reflectWaypoints(
+            makeWaypoint(3.419274091720581, 7.474021911621094, 0.0)
+        )
     },
     neutralbump: {
         leftWaypoints: [
@@ -217,8 +245,16 @@ const locationParams: Record<GenericLocation, LocationParams> = {
             makeWaypoint(5.955106735229492, 5.9437713623046875, -1.5707963267948966),
             makeWaypoint(5.971709251403809, 4.325946807861328, -1.6095124425448013),
             makeWaypoint(5.905695915222168, 7.3172287940979, 3.131788918491988),
-            makeWaypoint(2.770551919937134, 7.13850736618042, 0, true, false),
+            makeWaypoint(2.770551919937134, 7.13850736618042, 0, true, false)
         ],
+        rightWaypoints: reflectWaypoints(
+            makeWaypoint(2.9307680130004883, 7.31110954284668, 0, true, false),
+            makeWaypoint(5.697521209716797, 7.402477741241455, -0.006293),
+            makeWaypoint(5.955106735229492, 5.9437713623046875, -1.5707963267948966),
+            makeWaypoint(5.971709251403809, 4.325946807861328, -1.6095124425448013),
+            makeWaypoint(5.905695915222168, 7.3172287940979, 3.131788918491988),
+            makeWaypoint(2.770551919937134, 7.13850736618042, 0, true, false)
+        ),
         constraints: [
             makeConstraint(5, -1, {type: "PointAt", props: {x: toExpr(4.624067783355713, "m"), y: toExpr(4.038748741149902, "m"), tolerance: toExpr(0.017, "rad"), flip: false}}),
             makeConstraint(5, -1, {type: "MaxVelocity", props: {max: toExpr(2, "m/s")}})
@@ -240,21 +276,33 @@ const locationParams: Record<GenericLocation, LocationParams> = {
             makeWaypoint(5.6979265213012695, 7.4296064376831055, 3.141592653589793),
             makeWaypoint(3.2754716873168945, 7.364711284637451, 3.141592653589793, true, false)
         ],
+        rightWaypoints: reflectWaypoints(
+            makeWaypoint(3.7149038314819336, 7.477536201477051, 0.0, true, false),
+            makeWaypoint(4.809019565582275, 7.465332508087158, 0.0),
+            makeWaypoint(7.484222412109375, 6.948218822479248, -1.4711286226200226),
+            makeWaypoint(7.468718528747559, 4.425135612487793, -2.0576957311828057),
+            makeWaypoint(5.6979265213012695, 7.4296064376831055, 3.141592653589793),
+            makeWaypoint(3.2754716873168945, 7.364711284637451, 3.141592653589793, true, false)
+        ),
         constraints: [
             makeConstraint(5, -1, {type: "PointAt", props: {x: toExpr(4.624067783355713, "m"), y: toExpr(4.038748741149902, "m"), tolerance: toExpr(0.017, "rad"), flip: false}}),
             makeConstraint(5, -1, {type: "MaxVelocity", props: {max: toExpr(2, "m/s")}})
         ],
         eventMarkers: [
+            makeEventMarker("stop shooting", 0, 0),
             makeEventMarker("pivot out", 1, 0),
             makeEventMarker("feed in", 2, 0),
             makeEventMarker("feed stop", 3, 0.4),
-            makeEventMarker("shoot", 4, 0)
+            makeEventMarker("shoot", 5, 0)
         ]
     },
     trench: {
         leftWaypoints: [
             makeWaypoint(2.7739968299865723, 6.4496846199035645, 0, true, false)
         ],
+        rightWaypoints: reflectWaypoints( 
+            makeWaypoint(2.7739968299865723, 6.4496846199035645, 0, true, false)
+        ),
         constraints: [
             makeConstraint(0, -1, {type: "MaxVelocity", props: {max: toExpr(2, "m/s")}})
         ]
@@ -266,6 +314,12 @@ const locationParams: Record<GenericLocation, LocationParams> = {
             makeWaypoint(0.4468134641647339, 5.474844455718994, -1.5707963267948966),
             makeWaypoint(1.004274845123291, 5.474844455718994, 0.0, true, false)
         ],
+        rightWaypoints: [
+            makeWaypoint(1.5006606578826904, 5.137444972991943, 0.0, true, false),
+            makeWaypoint(0.47465676069259644, 4.826406002044678, 1.5708),
+            makeWaypoint(0.44392159581184387, 6.838572978973389, 1.5708),
+            makeWaypoint(1.206515908241272, 6.921103477478027, -1.5707963267948966, true, false)
+        ],
         constraints: [
             makeConstraint(1, 2, {type: "KeepInLane", props: {tolerance: toExpr(0.03, "m")}}),
             makeConstraint(3, -1, {type: "PointAt", props: {x: toExpr(4.624067783355713, "m"), y: toExpr(4.038748741149902, "m"), tolerance: toExpr(0.071, "rad"), flip: false}}),
@@ -273,12 +327,12 @@ const locationParams: Record<GenericLocation, LocationParams> = {
         ],
         eventMarkers: [
             makeEventMarker("stop shooting", 0, 0),
+            makeEventMarker("pivot out", 0, 0),
             makeEventMarker("feed in", 1, 0),
-            makeEventMarker("feed stop", 3, 0),
+            makeEventMarker("feed stop", 3, 0.4),
             makeEventMarker("shoot", 3, 0)
         ]
     },
-    human: {},
     climb: {}
 }
 
@@ -329,7 +383,7 @@ var baseTraj: Trajectory = {
             {from: "first", data: {type: "StopPoint", props: {}}, enabled: true},
             {from: "last", data: {type: "StopPoint", props: {}}, enabled: true},
             {from: "first", to: "last", data: {type: "KeepOutCircle", props: {x: toExpr(4.625668669119477, "m"), y: toExpr(6.208901214599609, "m"), r: toExpr(0.826396949005302, "m")}}, enabled: true},
-            // TODO right side keep out circle
+            {from: "first", to: "last", data: {type: "KeepOutCircle", props: {x: toExpr(4.625668669119477, "m"), y: toExpr(1.8539987854, "m"), r: toExpr(0.826396949005302, "m")}}, enabled: true},
             {from: "first", to: "last", data: {type: "KeepInRectangle", props: {x: toExpr(0, "m"), y: toExpr(0, "m"), w: toExpr(16.541, "m"), h: toExpr(8.0629, "m")}}, enabled: true}
         ],
         targetDt: toExpr(0.05, "s")
@@ -349,9 +403,19 @@ var baseTraj: Trajectory = {
     events: []
 }
 
-var layout: Layout = loadLayout("layout");
 
-var traj : Trajectory = buildTrajectory(baseTraj, layout.name, layout.layout);
+async function main() {
+    const layoutName: string = await prompt("layout to generate: ");
 
-saveTrajectory(traj);
-generateTrajectory(traj);
+    var layout: Layout = loadLayout(layoutName);
+    var traj: Trajectory = buildTrajectory(baseTraj, layout.name, layout.layout);
+    
+    saveTrajectory(traj);
+
+    console.log(`saved layout (${traj.name}):\n${JSON.stringify(traj, null, 2)}`)
+    console.log(`\ngenerating trajectory`)
+
+    generateTrajectory(traj);
+}
+
+main();
