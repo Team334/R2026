4f33a0332ee71aca9b2dcafd73682c79c0b00e4f
PGgit08 <peter.mty8@gmail.com>
2026-05-01 07:51:21 -0400

    removed the offset + increased driveSOTM speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -338,7 +338,7 @@ public final class Constants {
     public static final AngularVelocity driverAngularVelocity = RadiansPerSecond.of(Math.PI * 2);
 
     public static final LinearVelocity driverTranslationalShootingVelocity =
-        MetersPerSecond.of(1.5);
+        MetersPerSecond.of(2.25);
 
     public static final LinearVelocity profileTranslationalVelocity = MetersPerSecond.of(1);
     public static final LinearAcceleration profileTranslationalAcceleration =
